46792807d7f0e299d188e0660ffdefeb25da456b
ferreira-zxk <daviferreiralima2008@gmail.com>
2026-04-13 13:38:52 -0400

    aula01ex17.php e aula01ex16.php

---
+++ aula01ex16.php
@@ -0,0 +1,13 @@
+<?php
+// Declaração de uma variável booleana com o valor verdadeiro
+$sim = TRUE;
+// Declaração de uma variável booleana com o valor falso
+$nao = FALSE;
+?> 
+
+ 
+<?php
+//aula01ex17.php
+// Declaração de uma variável NULL
+$nulo = NULL;
+?>
