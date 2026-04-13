345ab13cadf8d3cdc852f2ba644cd4a1af2696e6
ferreira-zxk <daviferreiralima2008@gmail.com>
2026-04-13 15:25:21 -0400

    aula02ex09.php

---
+++ aula02ex09.php
@@ -0,0 +1,8 @@
+<?php
+$valor1 = true;
+$valor2 = true;
+if($valor1 and $valor2)// Avalia se valor1 e também valor2 são verdadeiros
+echo 'Verdadeiro'; // Se ambos os valores forem verdadeiros realiza esta expressão
+else
+echo 'Falso'; // Caso um ou todos os valores sejam falsos realiza esta expressão
+?>
