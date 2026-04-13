8e9dd8f0d8eabfd2dadb5edc799d4bd8d53e0b1d
ferreira-zxk <daviferreiralima2008@gmail.com>
2026-04-13 13:30:57 -0400

    aula01ex14.php

---
+++ aula01ex14.php
@@ -0,0 +1,14 @@
+<?php
+// Declaração de um produto
+$produto = 'pizza';
+// Declaração de uma variável com String com aspas duplas
+$texto = "Ele \"comprou\" uma $produto no Joey's.";
+?>
+<html>
+<head>
+<title>String com aspas duplas no PHP</title>
+</head>
+<body>
+<p><?php echo $texto; ?></p>
+</body>
+</html>
