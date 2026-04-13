928709d58f9fd91e6988894a7bd8686e4f1ddd08
ferreira-zxk <daviferreiralima2008@gmail.com>
2026-04-13 14:19:03 -0400

    aula02ex08.php

---
+++ aula02ex07.php
@@ -0,0 +1,7 @@
+<?php
+$string1 = "Programando";
+$string2 = "em PHP.";
+$stringFinal = $string1 . " " . $string2;
+// string1 junta com "espaço" que junta com string2
+echo($stringFinal);
+?>
---
+++ aula02ex08.php
@@ -0,0 +1,23 @@
+<?php
+echo "<h2>Atribuição simples</h2>";
+$valor1 = 100; // Operador atribui o valor 100 à variável $valor1
+echo "Valor 1 é: $valor1 <br>"; //100
+echo "<h2>Atribuição com adição</h2>";
+$valor1 += 50; // Igual a valor1 = valor1 + 50
+echo "Valor 1 é: $valor1 <br>"; // 150
+echo "<h2>Atribuição com subtração</h2>";
+$valor1 -= 25; // Igual a valor1 = valor1 - 25
+echo "Valor 1 é: $valor1 <br>"; // 125
+echo "<h2>Atribuição com multiplicação</h2>";
+$valor1 *= 2; // Igual a valor1 = valor1 * 2
+echo "Valor 1 é: $valor1 <br>"; // 250
+echo "<h2>Atribuição com divisão</h2>";
+$valor1 /= 2; // Igual a valor1 = valor1 / 2
+echo "Valor 1 é: $valor1 <br>"; // 125
+echo "<h2>Atribuição com módulo</h2>";
+$valor1 %= 3; // Retorna o resto da divisão de 125 por 3
+echo "Valor 1 é: $valor1 <br>"; // 2
+echo "<h2>Atribuição com módulo</h2>";
+$valor1 .= 9999; // Junta o valor "2" com "9999" ficando 29999.
+echo "Valor 1 é: $valor1 <br>"; // 29999
+?>
