0358692c5c8a41fe153c6a3a405b21a611ec02b7
ferreira-zxk <daviferreiralima2008@gmail.com>
2026-04-13 14:10:38 -0400

    aula02ex06.php

---
+++ aula02ex06.php
@@ -0,0 +1,6 @@
+<?php
+$operador++; // A variável foi incrementada para mais 1 (um).
+echo $operador . "<br>";
+$operador++;
+echo $operador; // A variável já guarda o valor 1 e foi incrementada para mais 1 (um)
+?>
