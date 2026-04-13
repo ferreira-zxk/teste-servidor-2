802d6271be943a34b7cc54b58d292842775b4d42
ferreira-zxk <daviferreiralima2008@gmail.com>
2026-04-13 13:23:40 -0400

    aula01ex13.php

---
+++ aula01ex13.php
@@ -0,0 +1,10 @@
+<?php
+// Declaração de uma variável com String com aspas simples
+$texto = 'A caixa d\'agua está transbordando.';
+?>
+<html>
+<head>
+<title>String com aspas simples no PHP</title>
+</head>
+<body>
+<p><?php echo $texto; ?></p>
