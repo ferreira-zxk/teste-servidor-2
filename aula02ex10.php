b5c93328de01c18ee824eaeb0979c99296b12e5c
ferreira-zxk <daviferreiralima2008@gmail.com>
2026-04-13 15:27:43 -0400

    aula02ex10.php

---
+++ aula02ex10.php
@@ -0,0 +1,9 @@
+<?php
+$valor1 = true;
+$valor2 = false;
+if($valor1 or $valor2)// Verdadeiro se pelo menos um valor for verdadeiro
+echo 'Verdadeiro'; // Se um ou todos os valores forem verdadeiros, realiza esta
+expressão
+else
+echo 'Falso'; // Caso todos os valores forem falsos, realiza esta expressão
+?>
